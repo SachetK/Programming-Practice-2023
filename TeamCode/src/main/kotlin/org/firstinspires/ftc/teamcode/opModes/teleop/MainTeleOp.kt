556988cf21d8cee870bd9a46dfa78b66c9ef7bfc
SachetK <71898487+SachetK@users.noreply.github.com>
2023-07-15 18:47:24 -0400

    Intake

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opModes/teleop/MainTeleOp.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opModes/teleop/MainTeleOp.kt
@@ -1,9 +1,33 @@
 package org.firstinspires.ftc.teamcode.opModes.teleop
 
 import com.arcrobotics.ftclib.command.CommandOpMode
+import com.arcrobotics.ftclib.gamepad.GamepadEx
+import com.arcrobotics.ftclib.gamepad.GamepadKeys
+import com.arcrobotics.ftclib.hardware.motors.Motor
+import org.firstinspires.ftc.teamcode.commands.intake.IntakeCommand
+import org.firstinspires.ftc.teamcode.subsystems.intake.IntakeSubsystem
 
 class MainTeleOp : CommandOpMode() {
+    private lateinit var intakeMotor: Motor
+
+    private lateinit var intakeSubsystem: IntakeSubsystem
+
+    private lateinit var intakeCommand: IntakeCommand
+    private lateinit var outtakeCommand: IntakeCommand
+
+    private lateinit var driver: GamepadEx
+
     override fun initialize() {
-        TODO("Not yet implemented")
+        intakeMotor = Motor(hardwareMap, "intake")
+
+        intakeSubsystem = IntakeSubsystem(intakeMotor)
+
+        intakeCommand = IntakeCommand(intakeSubsystem, true)
+        outtakeCommand = IntakeCommand(intakeSubsystem, false)
+
+        driver = GamepadEx(gamepad1)
+
+        driver.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER).whenPressed(intakeCommand)
+        driver.getGamepadButton(GamepadKeys.Button.LEFT_BUMPER).whenPressed(outtakeCommand)
     }
 }
